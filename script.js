be674af9c02936e44e81ecc56b2411c36f650af3
Delfina Buil <delfinabuil2@gmail.com>
2026-03-03 16:14:19 -0300

    add projects

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+const text = "delfi";  
+const typedText = document.getElementById("typed-text");
+const cursor = document.querySelector(".cursor");
+let index = 0;
+
+function type() {
+    if (index < text.length) {
+        typedText.textContent += text[index];
+        index++;
+        setTimeout(type, 200);
+    } else {
+        //keep cursor blinking after typing finishes
+        cursor.classList.add("blink-after");
+    }
+}
+
+// Start typing when page loads
+window.addEventListener("DOMContentLoaded", () => {
+    setTimeout(() => {
+        type();
+    }, 350); 
+});
+
 function scrollToSection(id) {
     const section = document.getElementById(id);
     section.scrollIntoView({ behavior: "smooth" });
